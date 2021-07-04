50b6734ac38cc962d72fd8e2eea4ee3f2e04213a
Blake <gn4514@wayne.edu>
2021-07-03 23:36:31 -0400

    test

---
+++ murica.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is here for your oil')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user.name:
+        return
+    await message.add_reaction(':flag_us:')
+
+@bot.command(name='restart')
+async def restart(ctx):
+    await ctx.close()
+    await ctx.clear()
+    await ctx.start()
+
+bot.run(TOKEN)
